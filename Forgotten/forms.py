898906bc2d0172dab61f1695b83aacbb79614e04
Ftapp <fabian.tappendorf@stud.th-luebeck.de>
2024-06-17 23:12:50 +0200

    Added my project

---
+++ Forgotten/forms.py
@@ -0,0 +1,11 @@
+from django.forms import *
+from Forgotten.models import *
+
+
+class PlacesForm(ModelForm):
+    class Meta:
+        model = Places
+        exclude = ()
+        labels ={'story_field':'Zum Ort',
+                 'upload_picture':'Bild hinzufügen',
+                 'location_map':'Geo-Daten für die Karte hinzufügen'}
