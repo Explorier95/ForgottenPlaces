f67e53164caa1b2b54242d24a632ac92b6f9dfda
Ftapp <fabian.tappendorf@stud.th-luebeck.de>
2024-06-18 15:07:07 +0200

    Bedingtes Löschen nun Funktional.

--- ForgottenPlaces/urls.py
+++ ForgottenPlaces/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from Forgotten.views import get_place_list, place_details, place_delete
+from Forgotten.views import get_place_list, place_details, PlaceDelete  # place_delete
 
 urlpatterns = [
 
@@ -25,8 +25,10 @@ urlpatterns = [
                   path('places/', get_place_list, name='place_list'),
                   path('places/add/', place_details, name='add_places'),
                   path('places/edit/<int:pk>', place_details, name='edit_place'),
-                  path('places/place_delete/<int:pk>', place_delete, name='place_delete'),
-                  # path('place_delete/<int:pk>', place_delete, name="place-delete"),
-                  # path('place_delete/<int:id>/', place_delete, name='place_delete'),
-                  # path('places/edit/<int:pk>', place_delete, name='place-delete'),
+                  path('place/delete/<int:pk>/', PlaceDelete.as_view(), name='place-delete'),
+                  # Wenn Update benötigt wird ...
+                  # path('place/update/<int:pk>/', PlaceUpdate.as_view(), name='place-update'),
+                  # URL zum direkten löschen
+                  # -> path('places/place_delete/<int:pk>', place_delete, name='place_delete'),
+
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
