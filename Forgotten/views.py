facdd6a6b0f639156ef39f94502f7898b7f83344
Ftapp <fabian.tappendorf@stud.th-luebeck.de>
2024-06-19 18:51:23 +0200

    Registrierung, login_required hinzugefügt

--- Forgotten/views.py
+++ Forgotten/views.py
@@ -2,22 +2,50 @@ from django.shortcuts import render
 from django.contrib import messages
 from django.views.generic.edit import DeleteView, UpdateView
 from django.http import HttpResponseRedirect, FileResponse
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse
 from Forgotten.models import *
 from Forgotten.forms import *
 from .models import Places
 
 
-# Create your views here.
-
-
 # gibt die List-Elemente aus
+@login_required()
 def get_place_list(request):
     places = Places.objects.all().order_by('name')
 
-    return render(request, 'Forgotten/place_list.html', {'page_title': 'Meine Orte',
+    return render(request, 'Forgotten/place_list.html', {'page_title': ' Forgotten Places',
                                                          'Places': places, })
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'Abgemeldet')
+    return HttpResponseRedirect(reverse_lazy('login'))
+
+
+def register_user(request):
+    form = UserCreationForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'Erfolgreich angemeldet')
+            return HttpResponseRedirect(reverse_lazy('place_list'))
+        else:
+            form = UserCreationForm()
+    return render(request, 'Forgotten/register_user.html', {'form': form})
+
+
+# Willkommensbildschirm wenn man den Root aufruft
+def get_first_view(request):
+    return render(request, 'Forgotten/welcome_screen.html', {'page_title': 'Forgotten Places'})
 
 
 # Funktion zum direkten Löschen von List-Elementen
@@ -27,6 +55,7 @@ def get_place_list(request):
 #     return HttpResponseRedirect(reverse('place_list'))
 
 # Funktion zum Speichern von neuen List-Elementen
+@login_required()
 def place_details(request, pk=None):
     if pk:
         places = Places.objects.get(pk=pk)
@@ -48,6 +77,8 @@ def place_details(request, pk=None):
                                                             'form': form})
 
 
+# Theoretisch unsicher sollte man die PK,s
+# wissen nach alternativlösung suchen damit @login_required() eingesetzt werden kann
 class PlaceDelete(DeleteView):
     model = Places
     context_object_name = 'place'
--- ForgottenPlaces/urls.py
+++ ForgottenPlaces/urls.py
@@ -14,20 +14,35 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from Forgotten.views import get_place_list, place_details, PlaceDelete  # place_delete
+from Forgotten.views import get_place_list, place_details, PlaceDelete, get_first_view, logout_view, \
+    register_user  # place_delete
+from Forgotten.forms import UserLoginForm
+from django.contrib.auth import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-
+                  path('', get_first_view, name='welcome'),
                   path('admin/', admin.site.urls),
-                  path('members/', include('members.urls')),
-                  path('members/', include('django.contrib.auth.urls')),
+                  # path('members/', include('members.urls')),
+                  # path('members/', include('django.contrib.auth.urls')),
                   path('places/', get_place_list, name='place_list'),
                   path('places/add/', place_details, name='add_places'),
                   path('places/edit/<int:pk>', place_details, name='edit_place'),
                   path('place/delete/<int:pk>/', PlaceDelete.as_view(), name='place-delete'),
+                  # Für eigenes Login mit Bootstrap template etc...
+                  path(
+                      'login/',
+                      views.LoginView.as_view(
+                          template_name="Forgotten/login.html",
+                          authentication_form=UserLoginForm
+                      ),
+                      name='login'),
+                  path('logout/', logout_view, name='logout'),
+                  path('regist/', register_user, name='register')
+
                   # Wenn Update benötigt wird ...
                   # path('place/update/<int:pk>/', PlaceUpdate.as_view(), name='place-update'),
                   # URL zum direkten löschen
