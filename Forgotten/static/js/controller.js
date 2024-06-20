c3580ec693e75979ae4b2f929aae545160283f67
heyitsmeali <alissab@posteo.de>
2024-06-20 18:32:32 +0200

    javascript ausgelagert - pfad: static/js/controller.js

--- Forgotten/static/js/controller.js
+++ Forgotten/static/js/controller.js
@@ -3,13 +3,24 @@
 * Fabian Tappendorf & Alissa Baumeister
 *
 * */
-document.addEventListener("DOMContentLoaded", function() {
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWxpc3NhamIiLCJhIjoiY2x4bXpqYW5tMGRobTJpczY1ZmNzaTFlbSJ9.cGqpPHYr4ezHEYYkKxtAeA';
+
+document.addEventListener('DOMContentLoaded', function() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYWxpc3NhamIiLCJhIjoiY2x4bXpqYW5tMGRobTJpczY1ZmNzaTFlbSJ9.cGqpPHYr4ezHEYYkKxtAeA';
     var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
         // style: 'mapbox://styles/mapbox/satellite-streets-v10' satellite view
         center: [13.40, 52.52], //Longitude & Latitude
-        zoom: 7
+        zoom: 6,
     });
+
+    const layerList = document.getElementById('menu');
+    const inputs = layerList.getElementsByTagName('input');
+
+    for (const input of inputs) {
+        input.onclick = (layer) => {
+            const layerId = layer.target.id;
+            map.setStyle('mapbox://styles/mapbox/' + layerId);
+        };
+    }
 });
