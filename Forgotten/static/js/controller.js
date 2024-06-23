fa6c0a18ed5250ca9aa5efccebac0274bc6f4de7
heyitsmeali <alissab@posteo.de>
2024-06-23 19:09:31 +0200

    es gibt zwar keine fehlermeldungen mehr aber dafür auch noch keine m arker..hm schwieriger als gedacht ich mach morgen weiter :)

--- Forgotten/static/js/controller.js
+++ Forgotten/static/js/controller.js
@@ -5,39 +5,54 @@
 * */
 
 document.addEventListener('DOMContentLoaded', function() {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiYWxpc3NhamIiLCJhIjoiY2x4bXpqYW5tMGRobTJpczY1ZmNzaTFlbSJ9.cGqpPHYr4ezHEYYkKxtAeA';
-    var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
-        // style: 'mapbox://styles/mapbox/satellite-streets-v10' satellite view
-        center: [13.40, 52.52], //Longitude & Latitude
-        zoom: 6,
-    });
+    fetch('/static/js/token.json')
+        .then(response => response.json())
+        .then(config => {
+            mapboxgl.accessToken = config.token;
+            var map = new mapboxgl.Map({
+                container: 'map',
+                style: 'mapbox://styles/mapbox/streets-v11',
+                // style: 'mapbox://styles/mapbox/satellite-streets-v10' satellite view
+                center: [13.40, 52.52], //Longitude & Latitude
+                zoom: 6,
+            });
 
-    const layerList = document.getElementById('menu');
-    const inputs = layerList.getElementsByTagName('input');
+            //change view from streetview to satelliteview
+            const layerList = document.getElementById('menu');
+            if (layerList) {
+                const inputs = layerList.getElementsByTagName('input');
 
-    for (const input of inputs) {
-        input.onclick = (layer) => {
-            const layerId = layer.target.id;
-            map.setStyle('mapbox://styles/mapbox/' + layerId);
-        };
-    }
-});
+                for (const input of inputs) {
+                    input.onclick = (layer) => {
+                        const layerId = layer.target.id;
+                        map.setStyle('mapbox://styles/mapbox/' + layerId);
+                    };
+                }
+            }
 
 
-var places = document.querySelector('.place-item');
-//function that sets a marker on the map for a specific coordinate (longitude/latitude)
-//Array.prototype.forEach.call(places, (pl) => {
-places.forEach(function(pl) {
-    var name = pl.getAttribute('name');
-    var longitude = parseFloat(pl.getAttribute('lon'));
-    var latitude = parseFloat(pl.getAttribute('lat'));
+            var places = document.querySelectorAll('.place-item');
+            //function that sets a marker on the map for a specific coordinate (longitude/latitude)
+            //Array.prototype.forEach.call(places, (pl) => {
+            if (places.length > 0) {
+                places.forEach(function (pl) {
+                    var name = pl.getAttribute('data-name');
+                    var longitude = parseFloat(pl.getAttribute('data-lon'));
+                    var latitude = parseFloat(pl.getAttribute('data-lat'));
 
-    var marker = new mapboxgl.Marker()
-        .setLngLat([longitude, latitude])
-        .setPopup(new mapboxgl.Popup().setText(name)).addTo(map);
-    });
+                    console.log('Adding marker:', name, longitude, latitude);
 
+                    if (!isNaN(longitude) && !isNaN(latitude)) {
+                        var marker = new mapboxgl.Marker()
+                            .setLngLat([longitude, latitude])
+                            .setPopup(new mapboxgl.Popup().setText(name)).addTo(map);
+                    }
+                });
+            } else {
+                console.log("no place found :(");
+            }
+        })
+        .catch(error => console.log(error));
+        });
 
 
--- Forgotten/views.py
+++ Forgotten/views.py
@@ -10,6 +10,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse
 from Forgotten.models import *
 from Forgotten.forms import *
+from .geocoding import Geocoding
 from .models import Places
 import json
 import requests
@@ -68,7 +69,14 @@ def place_details(request, pk=None):
     if request.method == 'POST':
         form = PlacesForm(request.POST, files=request.FILES, instance=places)
         if form.is_valid():
-            form.save()
+            place = form.save(commit=False)
+
+            geocoder = Geocoding()
+            coordinates = geocoder.get_coordinates(place.location_map)
+            if coordinates:
+                place.longitude, place.latitude = coordinates
+
+            place.save()
             messages.success(request, 'Ort gespeichert.')
             return HttpResponseRedirect(reverse_lazy('place_list'))
         else:
@@ -110,59 +118,10 @@ class MapView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['mapbox_access_token'] = 'pk.my_mapbox_access_token'
+        context['places'] = Places.objects.all()
         return context
 
     def map_view(request):
         places = Places.objects.all()
         return render(request, 'map.html')
                    #   {'places:' places})
-
-"""
-class for geocoding 
-the main function of this class is to return latitude and longitude from city names
-it uses the mapbox geocoding api 
-"""
-
-#TODO: security key noch in den token.json --> sichrheit !!! wichtig dann methode noch so dass es funktioniert
-# TODO: viewonmap button, marker müssen noch funzen, grüner button bei location, farben css
-#TODO: bei map noch oben menü aktivieren
-class Geocoding:
-    #initialise the class and load the key from the json (to pretect the key)
-    def __init__(self):
-        with open('token.json') as f:
-            data = json.load(f)
-            self.api_key = data['api_key']
-
-    #function for returning coordinates
-    def get_coordinates(self, region_name):
-        url = f'https://api.mapbox.com/geocoding/v5/mapbox.places/{region_name}.json'
-        params = {
-            'access_token': self.api_key,
-            'limit': 1
-        }
-
-        response = requests.get(url, params=params)
-        data = response.json()
-
-        if 'features' in data and len(data['features'])> 0:
-            coordinates = data['features'][0]['geometry']['coordinates']
-            return coordinates
-        else:
-            return None
-
-
-def get_coordinates(region_name):
-    geolocator = Nominatim(user_agent="my-application")
-    region_name = Places.name
-    try:
-        location = geolocator.geocode(region_name)
-        if location:
-            lat = location.latitude
-            lon = location.longitude
-            return lat, lon
-        else:
-            return None, None
-    except Exception as e:
-        print(f'Fehler beim geocoding: {e}')
-        return None, None
-
