735dda976d4619c4647d4d699966712bc6ed20cf
heyitsmeali <alissab@posteo.de>
2024-06-27 19:48:07 +0200

    gitignore und zoomen funktioniert jetzt endlich auch :)

--- Forgotten/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from Forgotten.models import*
-
-# Register your models here.
-
-admin.site.register(Account)
-admin.site.register(Places)
-admin.site.register(Category)
